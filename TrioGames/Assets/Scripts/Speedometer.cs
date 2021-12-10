6fbf631bf62cbf1f61c824dd08394c2822e26a47
Cem Orta <cem.orta@hotmail.com>
2021-12-10 08:02:08 +0300

    Speedometer v1
    Speedometer speed texts were added.
    Speed text was moved into the speedometer.

--- TrioGames/Assets/Scripts/Speedometer.cs
+++ TrioGames/Assets/Scripts/Speedometer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Speedometer : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class Speedometer : MonoBehaviour
     private float totalAngleSize = Mathf.Abs(ZERO_SPEED_ANGLE - MAX_SPEED_ANGLE);
     private float angleVersusSpeedRatio;
     private Transform needleTransform;
+    private Transform speedLabelTemplateTransform;
 
     // Car variables
     private float maxCarSpeed;
@@ -20,7 +22,11 @@ public class Speedometer : MonoBehaviour
         maxCarSpeed = playerCar.GetComponent<CarController>().maxSpeed;
         angleVersusSpeedRatio = totalAngleSize / maxCarSpeed;
         needleTransform = transform.Find("IMG_Needle");
+        speedLabelTemplateTransform = transform.Find("speedLabelTemplate");
+        speedLabelTemplateTransform.gameObject.SetActive(false);
         needleTransform.eulerAngles = new Vector3(0, 0, ZERO_SPEED_ANGLE);
+
+        CreateSpeedLabels();
     }
 
     private void FixedUpdate()
@@ -32,5 +38,22 @@ public class Speedometer : MonoBehaviour
     private float SetNeedle()
     {
         return ZERO_SPEED_ANGLE - Mathf.Abs(angleVersusSpeedRatio * currentCarSpeed);
+    }
+
+    private void CreateSpeedLabels()
+    {
+        int labelAmount = 10;
+
+        for (int i = 0; i < labelAmount; i++)
+        {
+            Transform speedLabelTransform = Instantiate(speedLabelTemplateTransform, transform);
+            float currentLabelOverLabelAmount = (float) i / (labelAmount - 1);
+            float speedLabelAngle = ZERO_SPEED_ANGLE - totalAngleSize * currentLabelOverLabelAmount;
+            speedLabelTransform.eulerAngles = new Vector3(0, 0, speedLabelAngle);
+            speedLabelTransform.Find("speedText").GetComponent<TMP_Text>().text = Mathf.RoundToInt(currentLabelOverLabelAmount * maxCarSpeed).ToString();
+            speedLabelTransform.Find("speedText").eulerAngles = Vector3.zero;
+            speedLabelTransform.gameObject.SetActive(true);
+        }
+        needleTransform.SetAsLastSibling();
     }
 }
