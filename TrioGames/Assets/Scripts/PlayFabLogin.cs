a48a291741e9f629984e9f3155780163d6322afe
Cem Orta <cem.orta@hotmail.com>
2021-12-17 11:17:15 +0300

    Revert "PlayFab Login & Signin"
    This reverts commit 3a3e9f0df4580d738e75127d7fc85d72498a443a.

--- TrioGames/Assets/Scripts/PlayFabLogin.cs
+++ TrioGames/Assets/Scripts/PlayFabLogin.cs
@@ -1,70 +1,254 @@
-using PlayFab;
-using PlayFab.ClientModels;
+/*using PlayFab;
+using PlayFab.Internal;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
-using TMPro;
-using UnityEngine.SceneManagement;
 
 public class PlayFabLogin : MonoBehaviour
 {
-    [SerializeField] TMP_InputField loginUsername;
-    [SerializeField] TMP_InputField loginEmail;
-    [SerializeField] TMP_InputField loginPassword;
-    [SerializeField] TMP_InputField signinUsername;
-    [SerializeField] TMP_InputField signinEmail;
-    [SerializeField] TMP_InputField signinPassword;
-    private String userEmail;
-    private String userPassword;
-    private String username;
     public string entityId; // Id representing the logged in player
     public string entityType; // entityType representing the logged in player
+    private readonly Dictionary<string, string> _entityFileJson = new Dictionary<string, string>();
+    private readonly Dictionary<string, string> _tempUpdates = new Dictionary<string, string>();
+    public string ActiveUploadFileName;
+    public string NewFileName;
+    // GlobalFileLock provides is a simplistic way to avoid file collisions, specifically designed for this example.
+    public int GlobalFileLock = 0;
 
-    public void OnClickLogin()
+    void OnLoginFailure(PlayFabError error)
     {
-        userEmail = loginEmail.text;
-        userPassword = loginPassword.text;
-        var request = new LoginWithEmailAddressRequest { Email = userEmail, Password = userPassword };
-        PlayFabClientAPI.LoginWithEmailAddress(request, OnLogInSuccess, OnLogInFailure);
+        Debug.LogError(error.GenerateErrorReport());
     }
-    void OnLogInSuccess(LoginResult result)
+
+    // OnGUI provides a way to build a Unity GUI entirely within script.
+    // Your GUI will be game-specific.
+    void OnGUI()
+    {
+        if (!PlayFabClientAPI.IsClientLoggedIn() && GUI.Button(new Rect(0, 0, 100, 30), "Login"))
+            Login();
+        if (PlayFabClientAPI.IsClientLoggedIn() && GUI.Button(new Rect(0, 0, 100, 30), "LogOut"))
+            PlayFabClientAPI.ForgetAllCredentials();
+        if (!PlayFabClientAPI.IsClientLoggedIn() && GUI.Button(new Rect(100, 0, 100, 30), "Sign In"))
+            //LoadAllFiles();
+
+        if (PlayFabClientAPI.IsClientLoggedIn())
+        {
+            // Display existing files
+            _tempUpdates.Clear();
+            var index = 0;
+            foreach (var each in _entityFileJson)
+            {
+                GUI.Label(new Rect(100 * index, 60, 100, 30), each.Key);
+                var tempInput = _entityFileJson[each.Key];
+                var tempOutput = GUI.TextField(new Rect(100 * index, 90, 100, 30), tempInput);
+                if (tempInput != tempOutput)
+                    _tempUpdates[each.Key] = tempOutput;
+                if (GUI.Button(new Rect(100 * index, 120, 100, 30), "Save " + each.Key))
+                    UploadFile(each.Key);
+                index++;
+            }
+            // Apply any changes
+            foreach (var each in _tempUpdates)
+                _entityFileJson[each.Key] = each.Value;
+
+            // Add a new file
+            NewFileName = GUI.TextField(new Rect(100 * index, 60, 100, 30), NewFileName);
+            if (GUI.Button(new Rect(100 * index, 90, 100, 60), "Create " + NewFileName))
+                UploadFile(NewFileName);
+        }
+    }
+
+    void Login()
+    {
+        var request = new PlayFab.ClientModels.LoginWithCustomIDRequest
+        {
+            CustomId = SystemInfo.deviceUniqueIdentifier,
+            CreateAccount = true,
+        };
+        PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
+    }
+
+    void OnLoginSuccess(PlayFab.ClientModels.LoginResult result)
     {
         entityId = result.EntityToken.Entity.Id;
         entityType = result.EntityToken.Entity.Type;
-        SceneManager.LoadScene("MenuScene");
-    }
-    void OnLogInFailure(PlayFabError error)
-    {
-        Debug.LogError(error.GenerateErrorReport());
     }
 
-    public void OnClickSignIn()
+    
+
+    void OnGetFileMeta(PlayFab.DataModels.GetFilesResponse result)
     {
-        username = signinUsername.text;
-        userEmail = signinEmail.text;
-        userPassword = signinPassword.text;
-        // If password or email fields are not empty
-        if (!(string.IsNullOrEmpty(userEmail) || string.IsNullOrEmpty(userEmail) || string.IsNullOrEmpty(username)))
+        Debug.Log("Loading " + result.Metadata.Count + " files");
+
+        _entityFileJson.Clear();
+        foreach (var eachFilePair in result.Metadata)
         {
-            var registerRequest = new RegisterPlayFabUserRequest { Email = userEmail, Password = userPassword, Username = username};
-            PlayFabClientAPI.RegisterPlayFabUser(registerRequest, OnRegisterSuccess, OnRegisterFailure);
+            _entityFileJson.Add(eachFilePair.Key, null);
+            GetActualFile(eachFilePair.Value);
         }
-    }
-    void OnRegisterSuccess(RegisterPlayFabUserResult result)
-    {
-        Debug.Log("Registered");
-    }
-    void OnRegisterFailure(PlayFabError error)
-    {
-        Debug.LogError(error.GenerateErrorReport());
+        GlobalFileLock -= 1; // Finish GetFiles
     }
 
-    private void Start()
+    void GetActualFile(PlayFab.DataModels.GetFileMetadata fileData)
     {
-        if (string.IsNullOrEmpty(PlayFabSettings.TitleId))
+        GlobalFileLock += 1; // Start Each SimpleGetCall
+        PlayFabHttp.SimpleGetCall(fileData.DownloadUrl,
+            result => { _entityFileJson[fileData.FileName] = Encoding.UTF8.GetString(result); GlobalFileLock -= 1; }, // Finish Each SimpleGetCall
+            error => { Debug.Log(error); }
+        );
+    }
+
+    void UploadFile(string fileName)
+    {
+        if (GlobalFileLock != 0)
+            throw new Exception("This example overly restricts file operations for safety. Careful consideration must be made when doing multiple file operations in parallel to avoid conflict.");
+
+        ActiveUploadFileName = fileName;
+
+        GlobalFileLock += 1; // Start InitiateFileUploads
+        var request = new PlayFab.DataModels.InitiateFileUploadsRequest
         {
-            PlayFabSettings.TitleId = "BC6E3";
+            Entity = new PlayFab.DataModels.EntityKey { Id = entityId, Type = entityType },
+            FileNames = new List<string> { ActiveUploadFileName },
+        };
+        PlayFabDataAPI.InitiateFileUploads(request, OnInitFileUpload, OnInitFailed);
+    }
+
+    void OnInitFailed(PlayFabError error)
+    {
+        if (error.Error == PlayFabErrorCode.EntityFileOperationPending)
+        {
+            // This is an error you should handle when calling InitiateFileUploads, but your resolution path may vary
+            GlobalFileLock += 1; // Start AbortFileUploads
+            var request = new PlayFab.DataModels.AbortFileUploadsRequest
+            {
+                Entity = new PlayFab.DataModels.EntityKey { Id = entityId, Type = entityType },
+                FileNames = new List<string> { ActiveUploadFileName },
+            };
+            PlayFabDataAPI.AbortFileUploads(request, (result) => { GlobalFileLock -= 1; UploadFile(ActiveUploadFileName); }, OnSharedFailure); GlobalFileLock -= 1; // Finish AbortFileUploads
+            GlobalFileLock -= 1; // Failed InitiateFileUploads
         }
+        else
+            OnSharedFailure(error);
+    }
+
+    void OnInitFileUpload(PlayFab.DataModels.InitiateFileUploadsResponse response)
+    {
+        string payloadStr;
+        if (!_entityFileJson.TryGetValue(ActiveUploadFileName, out payloadStr))
+            payloadStr = "{}";
+        var payload = Encoding.UTF8.GetBytes(payloadStr);
+
+        GlobalFileLock += 1; // Start SimplePutCall
+        PlayFabHttp.SimplePutCall(response.UploadDetails[0].UploadUrl,
+            payload,
+            FinalizeUpload,
+            error => { Debug.Log(error); }
+        );
+        GlobalFileLock -= 1; // Finish InitiateFileUploads
+    }
+
+    void FinalizeUpload(byte[] data)
+    {
+        GlobalFileLock += 1; // Start FinalizeFileUploads
+        var request = new PlayFab.DataModels.FinalizeFileUploadsRequest
+        {
+            Entity = new PlayFab.DataModels.EntityKey { Id = entityId, Type = entityType },
+            FileNames = new List<string> { ActiveUploadFileName },
+        };
+        PlayFabDataAPI.FinalizeFileUploads(request, OnUploadSuccess, OnSharedFailure);
+        GlobalFileLock -= 1; // Finish SimplePutCall
+    }
+    void OnUploadSuccess(PlayFab.DataModels.FinalizeFileUploadsResponse result)
+    {
+        Debug.Log("File upload success: " + ActiveUploadFileName);
+        GlobalFileLock -= 1; // Finish FinalizeFileUploads
     }
 }
+/*
+#if !DISABLE_PLAYFABENTITY_API && !DISABLE_PLAYFABCLIENT_API
+
+using PlayFab;
+using PlayFab.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class EntityFileExample : MonoBehaviour
+{
+    public string entityId; // Id representing the logged in player
+    public string entityType; // entityType representing the logged in player
+    private readonly Dictionary<string, string> _entityFileJson = new Dictionary<string, string>();
+    private readonly Dictionary<string, string> _tempUpdates = new Dictionary<string, string>();
+    public string ActiveUploadFileName;
+    public string NewFileName;
+    // GlobalFileLock provides is a simplistic way to avoid file collisions, specifically designed for this example.
+    public int GlobalFileLock = 0;
+
+    void OnSharedFailure(PlayFabError error)
+    {
+        Debug.LogError(error.GenerateErrorReport());
+        GlobalFileLock -= 1;
+    }
+
+    // OnGUI provides a way to build a Unity GUI entirely within script.
+  - // Your GUI will be game-specific.
+    void OnGUI()
+    {
+        if (!PlayFabClientAPI.IsClientLoggedIn() && GUI.Button(new Rect(0, 0, 100, 30), "Login"))
+            Login();
+        if (PlayFabClientAPI.IsClientLoggedIn() && GUI.Button(new Rect(0, 0, 100, 30), "LogOut"))
+            PlayFabClientAPI.ForgetAllCredentials();
+
+        if (PlayFabClientAPI.IsClientLoggedIn() && GUI.Button(new Rect(100, 0, 100, 30), "(re)Load Files"))
+            LoadAllFiles();
+
+        if (PlayFabClientAPI.IsClientLoggedIn())
+        {
+            // Display existing files
+            _tempUpdates.Clear();
+            var index = 0;
+            foreach (var each in _entityFileJson)
+            {
+                GUI.Label(new Rect(100 * index, 60, 100, 30), each.Key);
+                var tempInput = _entityFileJson[each.Key];
+                var tempOutput = GUI.TextField(new Rect(100 * index, 90, 100, 30), tempInput);
+                if (tempInput != tempOutput)
+                    _tempUpdates[each.Key] = tempOutput;
+                if (GUI.Button(new Rect(100 * index, 120, 100, 30), "Save " + each.Key))
+                    UploadFile(each.Key);
+                index++;
+            }
+            // Apply any changes
+            foreach (var each in _tempUpdates)
+                _entityFileJson[each.Key] = each.Value;
+
+            // Add a new file
+            NewFileName = GUI.TextField(new Rect(100 * index, 60, 100, 30), NewFileName);
+            if (GUI.Button(new Rect(100 * index, 90, 100, 60), "Create " + NewFileName))
+                UploadFile(NewFileName);
+        }
+    }
+
+    void Login()
+    {
+        var request = new PlayFab.ClientModels.LoginWithCustomIDRequest
+        {
+            CustomId = SystemInfo.deviceUniqueIdentifier,
+            CreateAccount = true,
+        };
+        PlayFabClientAPI.LoginWithCustomID(request, OnLogin, OnSharedFailure);
+    }
+
+    void OnLogin(PlayFab.ClientModels.LoginResult result)
+    {
+        entityId = result.EntityToken.Entity.Id;
+        entityType = result.EntityToken.Entity.Type;
+    }
+
+    
+}
+#endif
+*/
