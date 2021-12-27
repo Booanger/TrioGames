5f0ffaddbf4264c43804695b58dc5bed59ad5367
Cem Orta <cem.orta@hotmail.com>
2021-12-27 11:00:15 +0300

    Local data save script, Level Selection Scene

---
+++ TrioGames/Assets/Scripts/DataSaver.cs
@@ -0,0 +1,149 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+public class DataSaver
+{
+    //Save Data
+    public static void saveData<T>(T dataToSave, string dataFileName)
+    {
+        string tempPath = Path.Combine(Application.persistentDataPath, "data");
+        tempPath = Path.Combine(tempPath, dataFileName + ".txt");
+
+        //Convert To Json then to bytes
+        string jsonData = JsonUtility.ToJson(dataToSave, true);
+        byte[] jsonByte = Encoding.ASCII.GetBytes(jsonData);
+
+        //Create Directory if it does not exist
+        if (!Directory.Exists(Path.GetDirectoryName(tempPath)))
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(tempPath));
+        }
+        //Debug.Log(path);
+
+        try
+        {
+            File.WriteAllBytes(tempPath, jsonByte);
+            Debug.Log("Saved Data to: " + tempPath.Replace("/", "\\"));
+        }
+        catch (Exception e)
+        {
+            Debug.LogWarning("Failed To PlayerInfo Data to: " + tempPath.Replace("/", "\\"));
+            Debug.LogWarning("Error: " + e.Message);
+        }
+    }
+
+    //Load Data
+    public static T loadData<T>(string dataFileName)
+    {
+        string tempPath = Path.Combine(Application.persistentDataPath, "data");
+        tempPath = Path.Combine(tempPath, dataFileName + ".txt");
+
+        //Exit if Directory or File does not exist
+        if (!Directory.Exists(Path.GetDirectoryName(tempPath)))
+        {
+            Debug.LogWarning("Directory does not exist");
+            return default(T);
+        }
+
+        if (!File.Exists(tempPath))
+        {
+            Debug.Log("File does not exist");
+            return default(T);
+        }
+
+        //Load saved Json
+        byte[] jsonByte = null;
+        try
+        {
+            jsonByte = File.ReadAllBytes(tempPath);
+            Debug.Log("Loaded Data from: " + tempPath.Replace("/", "\\"));
+        }
+        catch (Exception e)
+        {
+            Debug.LogWarning("Failed To Load Data from: " + tempPath.Replace("/", "\\"));
+            Debug.LogWarning("Error: " + e.Message);
+        }
+
+        //Convert to json string
+        string jsonData = Encoding.ASCII.GetString(jsonByte);
+
+        //Convert to Object
+        object resultValue = JsonUtility.FromJson<T>(jsonData);
+        return (T)Convert.ChangeType(resultValue, typeof(T));
+    }
+
+    public static bool deleteData(string dataFileName)
+    {
+        bool success = false;
+
+        //Load Data
+        string tempPath = Path.Combine(Application.persistentDataPath, "data");
+        tempPath = Path.Combine(tempPath, dataFileName + ".txt");
+
+        //Exit if Directory or File does not exist
+        if (!Directory.Exists(Path.GetDirectoryName(tempPath)))
+        {
+            Debug.LogWarning("Directory does not exist");
+            return false;
+        }
+
+        if (!File.Exists(tempPath))
+        {
+            Debug.Log("File does not exist");
+            return false;
+        }
+
+        try
+        {
+            File.Delete(tempPath);
+            Debug.Log("Data deleted from: " + tempPath.Replace("/", "\\"));
+            success = true;
+        }
+        catch (Exception e)
+        {
+            Debug.LogWarning("Failed To Delete Data: " + e.Message);
+        }
+
+        return success;
+    }
+}
+
+/*
+ * Save Data:
+
+PlayerInfo saveData = new PlayerInfo();
+saveData.life = 99;
+saveData.highScore = 40;
+
+//Save data from PlayerInfo to a file named players
+DataSaver.saveData(saveData, "players");
+
+ * Load Data:
+
+PlayerInfo loadedData = DataSaver.loadData<PlayerInfo>("players");
+if (loadedData == null)
+{
+    return;
+}
+
+//Display loaded Data
+Debug.Log("Life: " + loadedData.life);
+Debug.Log("High Score: " + loadedData.highScore);
+
+for (int i = 0; i < loadedData.ID.Count; i++)
+{
+    Debug.Log("ID: " + loadedData.ID[i]);
+}
+for (int i = 0; i < loadedData.Amounts.Count; i++)
+{
+    Debug.Log("Amounts: " + loadedData.Amounts[i]);
+}
+
+ * Delete Data:
+
+DataSaver.deleteData("players");
+*/
--- TrioGames/Assets/Scripts/LevelSelectController.cs
+++ TrioGames/Assets/Scripts/LevelSelectController.cs
@@ -1,24 +1,30 @@
+using PlayFab;
+using PlayFab.ClientModels;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelSelectController : MonoBehaviour
 {
-    // userlevel verisi veritabanndan gelecek
-    int userLevel = 3;
 
-    int i = 0;
+    // playerlevel verisi veritabanından gelecek
+    int playerLevel = 3;
     [SerializeField] GameObject levels;
     [SerializeField] Sprite LOCKED_LEVEL_SPRITE;
     GameObject[] allLevels;
+    PlayerInfo loadedData;
+
     void Awake()
     {
         allLevels = new GameObject[levels.transform.childCount];
+        /*loadedData = DataSaver.loadData<PlayerInfo>("player");
+        playerLevel = GetPlayerLevel(loadedData);*/
         GetAllLevels();
         LockLevels();
     }
     void GetAllLevels()
     {
+        int i = 0;
         foreach (Transform child in levels.transform)
         {
             allLevels[i] = child.gameObject;
@@ -28,10 +34,19 @@ public class LevelSelectController : MonoBehaviour
     void LockLevels()
     {
         GameObject image;
-        for (int i = allLevels.Length - 1; userLevel <= i; i--)
+        for (int i = allLevels.Length - 1; playerLevel <= i; i--)
         {
             image = allLevels[i].transform.Find("image").gameObject;
             image.GetComponent<UnityEngine.UI.Image>().sprite = LOCKED_LEVEL_SPRITE;
+            image.GetComponent<UnityEngine.UI.Button>().enabled = false;
         }
     }
+    /*int GetPlayerLevel(PlayerInfo loadedData)
+    {
+        if (loadedData == null)
+        {
+            return 1;
+        }
+        return loadedData.level;
+    }*/
 }
